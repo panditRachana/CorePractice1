f90332dec7142e081745c9742543427b66790b60
panditRachana <rachupandit93@gmail.com>
2022-12-21 15:33:26 +0530

    Initial Commit

---
+++ CorePractice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CorePractice1
+//
+//  Created by Rachana Pandit on 21/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
